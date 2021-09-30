847cc583a69c070af13648680e814983c5924d9b
tonybwatson <tonybwatson@gmail.com>
2021-09-30 13:37:11 -0400

    weather pulls from api, button click and enter working

--- js/main.js
+++ js/main.js
@@ -1,17 +1,52 @@
-const axios = require('axios');
-
-// Make a request for a user with a given ID
-axios.get('https://api.openweathermap.org/data/2.5/weather?zip=40509,us&appid=7269431df9a1ea794e395e90f530febd')
-    .then(function (response) {
-        // handle success
-        console.log(response);
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-    .then(function () {
-        // always executed
-    });
-
 //   API key 7269431df9a1ea794e395e90f530febd
+
+// variable for inputting zipcode to api url
+var zip = '';
+
+var btn = document.getElementById('btn');
+var button1 = document.getElementById('button1')
+var apiLink = 'https://api.openweathermap.org/data/2.5/weather?zip=40509,us&appid=7269431df9a1ea794e395e90f530febd'
+
+// on click, call api
+button1.addEventListener('click', apiCall)
+// on "return" pressed, act as though button clicked
+zipInput.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        button1.click();
+    }
+})
+
+// function to call api via axios and write to html
+// Make a request to api
+function apiCall() {
+    // replace text in zip with input value
+    zip = document.getElementById('zipInput').value;
+    // call function, replacing '40509' in apiLink with zip
+    axios.get(apiLink.replace('40509', zip))
+        .then(function (response) {
+            // handle success
+            console.log(response);
+            console.log(response.data.main.temp)
+            document.getElementById('error').style.display = 'none';
+            document.getElementById('weatherHere').style.display = 'block';
+            document.getElementById('city').innerHTML = response.data.name;
+            document.getElementById('temp').innerHTML = Math.round(response.data.main.temp) + 'K';
+            document.getElementById('tempC').innerHTML = (Math.round(response.data.main.temp - 273.15)) + '°C';
+            document.getElementById('tempF').innerHTML = (Math.round(response.data.main.temp - 273.15) * 9 / 5 + 32) + '°F';
+            document.getElementById('conditions').innerHTML = response.data.weather[0].main;
+        })
+        .catch(function (error) {
+            zipNotFound();
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+}
+
+function zipNotFound() {
+    document.getElementById('error').style.display = 'block';
+    document.getElementById('error').innerHTML = 'Please enter a valid Zip code.';
+    document.getElementById('weatherHere').style.display = 'none';
+}
--- pseudocode.md
+++ pseudocode.md
@@ -11,7 +11,7 @@ order of operations
 1. go to url (html page)
 2. onLoad (init)
     * set vars/state
-    * bind events/click handlers - button click, hit enter,
+    * bind events/click handlers - button click, hit enter, 
     * give input auto-focus
     START 
     * validation of data entered - defensive programming
@@ -27,3 +27,34 @@ order of operations
         .isBusy = false
         .errorMsg = ''
 4. SUBMIT(API)
+
+Functions
+    sendToApi()
+        * run call to api with zipcode pulled from zipInput
+
+    calculateKelvinToFarenheit()
+    calculateKelvinToCelsius()
+    
+Objects
+    zipInput
+        * limited to 5 digits, numbers only
+    button
+        * when clicked, sends zipInput information concatenated with url pieces to api and returns that info parsed into html
+
+Variables
+    * variable for each area text must be written - city, temperature (k, c, f), condition, other info
+    * zipInput - text from zipInput input
+
+load page
+    html displays
+
+    enter zip in input
+        button clicked 
+            - concatenate url pieces with zip 
+            - send request to api
+        IF request fails 
+            display error message
+        IF request is answered
+            parse json
+            display to page matched to variables
+        repeat when clicked again
